3aa6e23e46a0c7c6d0cfae57ceecef8113daf219
unknown <ezapanaz@unsa.edu.pe>
2025-10-11 11:42:18 -0500

    ¨version1¨

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import libreria as lib
+
+A = np.random.randint(-5,10,size=(3,3))
+print("jhgfd\n",A)
+lib.intercambiaFilas(A,0,2)
+print("jhgfd\n",A)
+lib.operacionFila(A,2,0,2) 
+print("jhgfd\n",A)
