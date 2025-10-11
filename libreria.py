17550165ba921511432ee34a4321f500bf7219da
unknown <ezapanaz@unsa.edu.pe>
2025-10-11 12:34:58 -0500

    fueron adicionadas funciones para escalonar matrices

--- libreria.py
+++ libreria.py
@@ -5,5 +5,25 @@ def intercambiaFilas(A,fil_i,fil_j):
 
 def operacionFila(A,fil_m,fil_piv,factor): #fil_m = fil_m. factor *fil_piv
     A[fil_m,:] = A[fil_m,:] - factor*A[fil_piv,:]
-#def reescalaFila(A,fil_m,factor):
- #   A[fil_m,:] = factor*A[fil_m,:]
+
+def reescalaFila(A,fil_m,factor):
+    A[fil_m,:] = factor*A[fil_m,:]
+
+def escalonaSimple(A):
+    nfil = A.shape[0]
+    ncol = A.shape[1]
+
+    for j in range(0,nfil):
+        for i in range(j+1,nfil):
+            ratio = A[i,j]/A[j,j]
+            operacionFila(A,i,j,ratio)
+
+def escalonaconPiv(A):
+    nfil = A.shape[0]
+    ncol = A.shape[1]
+    for j in range(0,nfil):
+        imax = np.argmax(np.abs(A[j:nfil,j]))
+        intercambiaFilas(A,j+imax,j)
+        for i in range (j+1,nfil):
+            ratio = A[i,j]/A[j,j]
+            operacionFila(A,i,j,ratio)
